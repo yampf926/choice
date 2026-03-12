f5519bffd34a9a740dfad1218012930a1c33a226
develckm86 <develckm86@gmail.com>
2026-03-12 16:53:20 +0900

    클래스 정리하고 다시 안만들게 지침추가

--- out/MainSmokeTest.java
+++
@@ -1,8 +0,0 @@
-public class MainSmokeTest {
-    public static void main(String[] args) throws Exception {
-        javax.swing.SwingUtilities.invokeAndWait(() -> {
-            Main main = new Main();
-            main.dispose();
-        });
-    }
-}
--- out_test/MainSmokeTest.java
+++
@@ -1,5 +0,0 @@
-public class MainSmokeTest {
-    public static void main(String[] args) throws Exception {
-        javax.swing.SwingUtilities.invokeAndWait(() -> new Main());
-    }
-}
